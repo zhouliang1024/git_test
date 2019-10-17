ba17c64f49f2525fa94d9f19ccd9e679028ae2ba
zl <zhouliang654@163.com>
2019-10-17 20:49:46 +0800

    添加工程到git

---
+++ src/main/java/cn/itcast/service/Test.java
@@ -0,0 +1,11 @@
+package cn.itcast.service;
+
+public class Test {
+    public static void main(String[] args) {
+        int a=1;
+        for (int i = 0; i < 10; i++) {
+            a+=i;
+        }
+        System.out.println(a);
+    }
+}
